bb6e3dce0925af4b7d405eb8733cf00cef6f83f3
Arpon Dutta <arpongen3094@gmail.com>
2025-08-15 21:10:54 +0530

    #

---
+++ components/CompanionList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const CompanionList = () => {
+  return (
+    <div>
+      CompanionList
+    </div>
+  )
+}
+
+export default CompanionList
