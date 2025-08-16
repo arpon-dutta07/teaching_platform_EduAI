0e6ac3b524dc037ecf29766025f5f59d34bdae19
Arpon Dutta <arpongen3094@gmail.com>
2025-08-16 14:49:04 +0530

    Refactor layout in page.tsx for improved responsiveness and styling

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <main>
-      <article>
+    <main className='min-lg:w-1/3 min-md:w-2/3  items-center justify-center'>
+      <article className='w-full flex-col gap-4'>
         <h1>Companion Builder</h1>
 
         <CompanionForm/>
