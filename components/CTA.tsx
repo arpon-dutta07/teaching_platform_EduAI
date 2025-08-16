1d5c7628e5c051dab427a971ac8c418f2d61259a
Arpon Dutta <arpongen3094@gmail.com>
2025-08-16 14:49:26 +0530

    Implement CTA component with structured layout and interactive elements

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
-    </div>
+    <section className="cta-section">
+      <div className="cta-badge">
+        Start learning your way.
+      </div>
+      <h2 className='text-3xl font-bold'>
+        Build and personalize learning companion.
+      </h2>
+      <p>
+        Pick a name, subject, voice, & personality and
+        let your companion guide you through personalized lessons, that feels natural and fun.
+      </p>
+      <Image
+        src="/images/cta.svg"
+        alt="CTA Illustration"
+        width={362}
+        height={232}
+        className="cta-illustration"
+      />
+
+      <button className='btn-primary'>
+        <Image
+        src={"icons/plus.svg"}
+        alt="Plus Icon"
+        width={12}  
+        height={12}        
+        />
+        <Link href="/companions/new">
+          <p>
+            Build a New companion
+          </p>
+        </Link>
+      </button>
+    </section>
   )
 }
 
