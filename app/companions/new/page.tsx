4cf8883999109e20023b00cbaaf3c010a3fbd47b
Arpon Dutta <arpongen3094@gmail.com>
2025-08-16 20:23:30 +0530

    Rename page component to NewCompanion and implement user authentication redirect

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,14 @@
 import CompanionForm from '@/components/CompanionForm'
+import {redirect} from "next/navigation";
+import {auth} from "@clerk/nextjs/server";
 import React from 'react'
 
-const page = () => {
+const NewCompanion  = async () => {
+  const {userId} = await auth();
+  if(!userId) {
+    redirect('/sign-in');
+  }
+
   return (
     <main className='min-lg:w-1/3 min-md:w-2/3  items-center justify-center'>
       <article className='w-full flex-col gap-4'>
@@ -13,4 +20,4 @@ const page = () => {
   )
 }
 
-export default page
+export default NewCompanion
