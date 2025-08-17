3e2f29324a2ba6889ddafdda6b21651e65493918
Arpon Dutta <arpongen3094@gmail.com>
2025-08-17 13:19:56 +0530

    #

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { SmoothCursor } from "@/components/SmoothCursor";
 import { ClerkProvider } from "@clerk/nextjs";
+
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -10,7 +12,7 @@ const bricolage = Bricolage_Grotesque({
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
+  title: "EduAI",
   description: "Real-time AI Teaching Platform",
 };
 
@@ -20,13 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-        <ClerkProvider apperance={{ variables: { colorPrimary: "#fe5933" } }}>
-        <Navbar />
-        {children}
-        </ClerkProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+          <Navbar />
+          {children}
+          <SmoothCursor />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
